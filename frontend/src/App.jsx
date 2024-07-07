eea1eac541ab4ccb9d92e800d4ce3c864499a385
NehaTirumalasetti <ntirumalasetti@gmail.com>
2024-07-07 11:53:35 -0700

    added components for each page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,35 +5,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFilm } from '@fortawesome/free-solid-svg-icons'
 import Lottie from 'lottie-react'
 import animationData from './assets/video-film.json'
-// import FirstPage from './firstPage'
-
+import FirstPage from './FirstPage'
+import SecondPage from './SecondPage'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [uploadStatus, setUploadStatus] = useState('not uploaded')
+  const handleChange = () => {
+
+  }
   const songs = ['Count me out - Kendrick Lamar', 'So far away - Avenged Sevenfold', 'Won\'t go home without you - Maroon 5', 'Toxicity - System of a down', 'Seven Nation Army - The white stripes']
   return (
     <>
       <div className='root'>
       <h1>Ongaku</h1>
         <div className='upload-card'>
-          {/* <FirstPage/> */}
-          <div className='card'>
-          
-          <Lottie animationData={animationData} className='gif'/>
-          <label for="file-upload" class="custom-file-upload">
-          <FontAwesomeIcon icon={faFilm} />  
-          {' Upload Video'}
-          </label>
-          <input id="file-upload" type='file'></input>
-          {/* <button>Upload</button>*/}
-          </div> 
-          {/* <div className='table'>
-            <h2>Try one of these songs!</h2>
-              {songs.map((item) => (
-                <span>{item}</span>
-              ))}
-          </div>
-         <img src='https://cdn-0.studybreaks.com/wp-content/uploads/2022/11/IMG_2547.png?ezimgfmt=rs:387x258/rscb8/ngcb8/notWebP'/> */}
+          {uploadStatus === 'not uploaded' && <FirstPage/>}
+          {/* Loading screeen for uploading */}
+          {uploadStatus === 'uploaded' && <SecondPage/>}
         </div>
       </div>
     </>
--- frontend/src/FirstPage.jsx
+++ frontend/src/FirstPage.jsx
@@ -1,8 +1,11 @@
 import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFilm } from '@fortawesome/free-solid-svg-icons'
+import Lottie from 'lottie-react'
+import animationData from './assets/video-film.json'
 
 function FirstPage(){
     return(
-        <>
         <div className='card'>
           
           <Lottie animationData={animationData} className='gif'/>
@@ -12,8 +15,7 @@ function FirstPage(){
           </label>
           <input id="file-upload" type='file'></input>
           {/* <button>Upload</button> */}
-          </div>
-        </>
+        </div>
     )
 }
 export default FirstPage
