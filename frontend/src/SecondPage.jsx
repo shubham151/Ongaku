a058cdda009682a5f1227499c25e9e355897f565
NehaTirumalasetti <ntirumalasetti@gmail.com>
2024-07-07 19:20:41 -0700

    thumbnail carousel

--- frontend/src/SecondPage.jsx
+++ frontend/src/SecondPage.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 
 function SecondPage({ songs }){
@@ -6,11 +8,22 @@ function SecondPage({ songs }){
     return(
         <><div className='table'>
             <h2>Try one of these songs!</h2>
-            {songs.map((item) => (
-                <span>{item}</span>
-            ))}
+            <Carousel>
+            {songs.map((item,index) => (
+                // <div className='song'>
+                // <img className='thumbnail'src={item.image} />
+                // <span>{item.songName + " - " + item.artist}</span>
+                // </div>
+                  <div key={index}>
+                    <img src={item.image} alt={`Slide ${index}`} />
+                    <a className="legend">{item.songName + " - " + item.artist}</a>
+                  </div>
+                ))}
+              </Carousel>
+            {/* ))} */}
         </div>
-        <img src='https://cdn-0.studybreaks.com/wp-content/uploads/2022/11/IMG_2547.png?ezimgfmt=rs:387x258/rscb8/ngcb8/notWebP' /></>
+        {/* <img className= 'side' src='https://cdn-0.studybreaks.com/wp-content/uploads/2022/11/IMG_2547.png?ezimgfmt=rs:387x258/rscb8/ngcb8/notWebP' /> */}
+        </>
     )
 }
 export default SecondPage
